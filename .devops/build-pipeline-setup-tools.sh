a76a6e629bd924dbaf6deb7f23a3776bda2cbcbd
devsimiyu <dev.wrodgerss@gmail.com>
2026-03-31 20:01:05 +0300

    feature/cmake-vcpkg-toolchain: Provide bash script for installing CMake latest version and Ninja on build pipeline runner

---
+++ .devops/build-pipeline-setup-tools.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# Install CMake and Ninja
+
+echo "📦 Setting up Kitware repository for latest CMake..."
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
+
+sudo apt update
+sudo apt install -y cmake ninja-build
